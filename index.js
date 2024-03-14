99d560c40db151a1ff463e51e82fb5267820c776
Panji Dinata <panjidinata88@gmail.com>
2024-03-14 16:43:45 +0800

    parse header

--- index.js
+++ index.js
@@ -2,29 +2,45 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/whoami", function (req, res) {
+  const ipAddress = req.ip;
+  const acceptLanguage = req.get("Accept-Language");
+  const userAgent = req.get("User-Agent");
+
+  console.log(`ip address: ${ipAddress}`);
+  console.log(`accept-language: ${acceptLanguage}`);
+  console.log(`user-agent: ${userAgent}`);
+
+  res.json({
+    ipaddress: ipAddress,
+    language: acceptLanguage,
+    software: userAgent,
+  });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
